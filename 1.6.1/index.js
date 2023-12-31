41e4fbcd76c56d29f06f08c6257e6a3f18cd914c
slin-sap <shen.lin@sap.com>
2023-12-31 12:04:50 -0800

    add req config log

--- 1.6.1/index.js
+++ 1.6.1/index.js
@@ -1,14 +1,22 @@
-import 'dotenv/config';
-import axios from 'axios';
+import "dotenv/config";
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.HOST
+  baseURL: process.env.HOST,
 });
 
+axiosInstance.interceptors.request.use(
+  function (config) {
+    console.log(config);
+    return config;
+  }
+);
+
 async function main() {
-    const response = await axiosInstance.get('/sap/public/bc/ui5_ui5/bootstrap_info.json');
-    console.log(response.status);
-    console.log(response.statusText);
+  const response = await axiosInstance.get(
+    "/sap/public/bc/ui5_ui5/bootstrap_info.json"
+  );
+  console.log(response.status, response.statusText);
 }
 
 main();
