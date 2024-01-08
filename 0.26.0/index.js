bb469555a5566edab51b94dd19f6817d90ecf513
slin-sap <shen.lin@sap.com>
2024-01-08 07:42:40 -0800

    use reponse log

--- 0.26.0/index.js
+++ 0.26.0/index.js
@@ -1,11 +1,25 @@
 import "dotenv/config";
 import axios from "axios";
+// import adl from 'axios-debug-log';
+
+// adl({
+//   request: function (debug, config) {
+//     debug('Request with ' + config)
+//   }
+// })
 
 const axiosInstance = axios.create({
   baseURL: process.env.HOST,
 });
 
 axiosInstance.interceptors.request.use(
+  function (config) {
+    // console.log(config);
+    return config;
+  }
+);
+
+axiosInstance.interceptors.response.use(
   function (config) {
     console.log(config);
     return config;
